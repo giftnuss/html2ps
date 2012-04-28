9cad39a9952f72d0a5ea52bf6473e5090ff296de
giftnuss <giftnuss@netscape.net>
2012-04-28 22:06:23 +0000

    cosmetic

--- tree.navigation.inc.php
+++ tree.navigation.inc.php
@@ -1,19 +1,23 @@
 <?php
 // $Header: /cvsroot/html2ps/tree.navigation.inc.php,v 1.13 2007/05/06 18:49:29 Konstantin Exp $
 
-class TreeWalkerDepthFirst {
+class TreeWalkerDepthFirst
+{
   var $_callback;
 
-  function TreeWalkerDepthFirst($callback) {
+  function TreeWalkerDepthFirst($callback) 
+  {
     $this->_callback = $callback;
   }
 
-  function run(&$node) {
+  function run(&$node) 
+  {
     call_user_func($this->_callback, array('node' => &$node));
     $this->walk_element($node);
   }
 
-  function walk_element(&$node) {
+  function walk_element(&$node) 
+  {
     if (!isset($node->content)) {
       return;
     };
@@ -25,12 +29,13 @@ class TreeWalkerDepthFirst {
   }
 }
 
-function &traverse_dom_tree_pdf(&$root) {
+function traverse_dom_tree_pdf(DOMTree $root)
+{
   switch ($root->node_type()) {
   case XML_DOCUMENT_NODE:
-    $child =& $root->first_child();
+    $child = $root->first_child();
     while($child) {
-      $body =& traverse_dom_tree_pdf($child);
+      $body = traverse_dom_tree_pdf($child);
       if ($body) { 
         return $body; 
       }
